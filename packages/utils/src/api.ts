355bf53b62cf83c1665f01f1b57ebfcde5c2ca18
Aniket Kulkarni <kaniket@mobileiron.com>
2021-09-06 00:26:01 +0530

    feat(eeg): cwdwdw

--- packages/utils/src/api.ts
+++ packages/utils/src/api.ts
@@ -31,7 +31,7 @@ export function useAsyncDataEffect<T>(
         if (!cancelled) {
           console.info(
             "%c Updating state: " + stateName,
-            "background: green; color: #fff; display: block;"
+            "background: green; color: white; display: block;"
           );
           setter(data);
         }
